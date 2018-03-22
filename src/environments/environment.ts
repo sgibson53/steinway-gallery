ddccf424869e9fda452fefc2892f9f5a2fb63224
Seth Gibson <wgibson53@gmail.com>
2018-03-22 11:13:30 -0500

    From two days ago

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA27izgiRRoOgouEsciAU8fRzgcPQnAfNc',
+    authDomain: 'steinway-gallery-2ee6a.firebaseapp.com',
+    databaseURL: 'https://steinway-gallery-2ee6a.firebaseio.com',
+    projectId: 'steinway-gallery-2ee6a',
+    storageBucket: '',
+    messagingSenderId: '491872175744'
+  }
 };
